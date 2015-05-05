7b2f206ba1b51f1bade1662737b58dc8cb01c22d
Jean-Baptiste Note <jb.note@criteo.com>
2015-05-05 13:11:35 +0200

    Adjust deploy script to account for new naming

--- app-packages/storm/deploy.sh
+++ app-packages/storm/deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo "Remember to customize appConfig-default.json and possibly resources-default.json"
 APPNAME=${1:-storm-on-slider}
-slider install-package --replacepkg --name STORM --package target/slider-storm-app-package-0.70.1-incubating.zip
+slider install-package --replacepkg --name STORM --package target/slider-storm-app-package-0.9.4.zip
 slider stop $APPNAME
 slider destroy $APPNAME 
-slider create $APPNAME --filesystem hdfs://root --queue dev --template target/slider-storm-app-package-0.70.1-incubating/appConfig-default.json --resources target/slider-storm-app-package-0.70.1-incubating/resources-default.json -D fs.defaultFS=hdfs://root
+slider create $APPNAME --filesystem hdfs://root --queue dev --template target/slider-storm-app-package-0.9.4/appConfig-default.json --resources target/slider-storm-app-package-0.9.4/resources-default.json -D fs.defaultFS=hdfs://root
